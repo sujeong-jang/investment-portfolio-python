b3c25f0e8c771f1ae92ded8d8f9db9d4ad783332
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2021-08-02 14:43:35 +0900

    과거 수익률 및 백테스팅 차트

--- portfolioapp/run.py
+++ portfolioapp/run.py
@@ -4,6 +4,7 @@ import numpy as np
 import datetime
 import sqlalchemy as sa
 import cx_Oracle
+import pandas as pd
 
 from flask import Flask, render_template, session, request, redirect, url_for
 
@@ -60,13 +61,26 @@ def survey():
 @app.route("/portfolio", methods=['POST', 'GET'])
 def portfolio():
     if request.method == 'POST':
-        money = request.form['money']
-        type = request.form['type']
+        # 국내
+        portfolio0 = ['195930', '133690', '273130', '284430', '183700']  # 안정형
+        portfolio1 = ['195930', '133690', '239660', '284430', '183700']  # 안정추구형
+        portfolio2 = ['195930', '133690', '239660', '278620', '284430']  # 위험중립형
+        portfolio3 = ['195930', '278530', '133690', '239660', '284430']  # 적극투자형
+        portfolio4 = ['195930', '278530', '277630', '133690', '284430']  # 공격투자형
+
+        # 미국
+        portfolio5 = ['OILK', 'BBJP', 'ARKK', 'PALL', 'QQQ']  # 안정형
+        portfolio6 = ['OILK', 'SPHB', 'BBJP', 'ARKK', 'PALL']  # 안정추구형
+        portfolio7 = ['OILK', 'SPHB', 'JAGG', 'BBJP', 'ARKK']  # 위험중립형
+        portfolio8 = ['OILK', 'BBCA', 'SPHB', 'JAGG', 'ARKK']  # 적극투자형
+        portfolio9 = ['OILK', 'BBCA', 'BBEU', 'BBJP', 'ARKK']  # 공격투자형
+
+        price = request.form['price']
+        invest_type = request.form['type']
         risk_no = request.form['risk_no']
         is_oversea = request.form['oversea']
 
         db = ""
-
 
         if is_oversea == '0': # 해외 ETF
             db = "ETF_US"
@@ -78,7 +92,7 @@ def portfolio():
         with oracle_engine.connect() as conn:
             try:
                 sql = "select * from " +  db + " where invest_type=:1"
-                results = conn.execute(sql, (type)).fetchall()
+                results = conn.execute(sql, (invest_type)).fetchall()
 
                 name_list = [] # 상품명
                 risk_list = [] # 위험등급
@@ -95,9 +109,64 @@ def portfolio():
                     returns_3y.append(etf[5])
                     returns_5y.append(etf[6])
 
+                # 투자성향 상품별 과거 수익률 데이터 가져오기
+                sql = "select * from RETURN"
+                return_df = pd.read_sql(sql, conn)
+
+                etf_list = []
+                return_list = {}
+                date_list = {}
+
+                if is_oversea == '0':  # 해외
+                    if invest_type == '안정형':
+                        portfolio_data = portfolio5
+                    elif invest_type == '안정추구형':
+                        portfolio_data = portfolio6
+                    elif invest_type == '위험중립형':
+                        portfolio_data = portfolio7
+                    elif invest_type == '적극투자형':
+                        portfolio_data = portfolio8
+                    else:
+                        portfolio_data = portfolio9
+                else:
+                    if invest_type == '안정형':
+                        portfolio_data = portfolio0
+                    elif invest_type == '안정추구형':
+                        portfolio_data = portfolio1
+                    elif invest_type == '위험중립형':
+                        portfolio_data = portfolio2
+                    elif invest_type == '적극투자형':
+                        portfolio_data = portfolio3
+                    else:
+                        portfolio_data = portfolio4
+
+                for i, ticker in enumerate(portfolio_data):
+                    name = return_df[return_df['ticker'] == ticker]['name'].unique().tolist()[0]
+                    if name not in etf_list:
+                        etf_list.append(name)
+
+                    return_list[i] = list(return_df[return_df['ticker'] == ticker]['return'].map(float).values)
+                    date_list[i] = list(
+                        return_df[return_df['ticker'] == ticker]['rdate'].dt.strftime('%Y-%m-%d').unique())
+
+                # 포트폴리오 수익률 데이터 가져오기
+                if is_oversea == '0':  # 해외
+                    sql = "select * from pf_us"
+                    pf_df = pd.read_sql(sql, conn)
+                    pf_df = pf_df[46:]
+                else:  # 국내
+                    sql = "select * from pf_kr"
+                    pf_df = pd.read_sql(sql, conn)
+                    pf_df = pf_df[140:]
+
+                pf_list = pf_df[invest_type].map(float).tolist()
+
+                bt_data = []
+                for i, pf in enumerate(pf_list):
+                    bt_data.append({'x': i, 'y': pf});
+
             except Exception as e:
                 print(e)
-                return render_template('error.html')
 
         # 투자 등급 카운팅 (파이차트에 비중 나타내기 위해 사용)
         count_list = [0,0,0]
@@ -110,7 +179,11 @@ def portfolio():
             else:
                 count_list[2] += 1
 
-        return render_template('portfolio.html', KEY_INVEST_TYPE=type, KEY_NAME_LIST=name_list, KEY_RISK_LIST=risk_list, KEY_WEIGHT_LIST=weight_list, KEY_COUNT_LIST=count_list, KEY_RETURN_1Y=returns_1y, KEY_RETURN_3Y=returns_3y, KEY_RETURN_5Y=returns_5y)
+        return render_template('portfolio.html', KEY_PRICE=price, KEY_INVEST_TYPE=invest_type, KEY_NAME_LIST=name_list,
+                               KEY_RISK_LIST=risk_list, KEY_WEIGHT_LIST=weight_list, KEY_COUNT_LIST=count_list,
+                               KEY_RETURN_1Y=returns_1y, KEY_RETURN_3Y=returns_3y, KEY_RETURN_5Y=returns_5y,
+                               KEY_ETF_LIST=etf_list, KEY_RETURN_LIST=return_list, KEY_DATE_LIST=date_list,
+                               KEY_BACKTESTING=bt_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
